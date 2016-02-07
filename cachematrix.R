a9d668f7c381a39deb57d2cd7d86ba196fcd1654
Lewis Campbell <lewis.campbell@gmail.com>
2016-02-07 16:55:43 +0930

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This firstly generated a NULL "Matrix" to hold future values
+## then populated the matrix with the call values and assigned to the global
+## environment using superassignment operator.  The solve function is called on
+## the (initially null) matrix to calculate its inverse, which is then set as _m_.
 
 ## set matrix, get the value, set the inverse of the matrix and get that inverse.
 
 makeCachematrix <- function(x = matrix()) {
         m <- NULL
-        set <- function(y) {
+        setmat <- function(y) {
             x <<- y
             m <<- NULL
         }
-        get <- function() x
+        getmat <- function() x
         setinverse <- function(solve) m <<- solve
         getinverse <- function() m
-        list(set = set, get = get,
+        list(set = setmat, get = getmat,
              setinverse = setinverse,
              getinverse = getinverse)
 }
 
 
-## Checks for cached regenerated original matrix and if not present calculates
-## the original matrix by reinversion
+## Checks for cached regenerated original matrix; if present returns it,
+## if not present calculates the original matrix by reinversion,
+## then returns that, in either case naming the returned matrix _m_ 
 
 cachesolve <- function(x, ...) {
-    m <- x$getsolve()
+    m <- x$getinverse()
     if(!is.null(m)) {
         message("getting cached data")
         return(m)
     }
     data <- x$get()
     m <- solve(data, ...)
-    x$setsolve(m)
+    x$setinverse(m)
     m
 }
